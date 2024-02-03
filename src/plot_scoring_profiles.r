155a194b27f4187f8362384b0b8be318a7ddc22f
MrAli1582 <139212688+MrAli1582@users.noreply.github.com>
2024-02-03 09:04:17 +0100

    Update plot_scoring_profiles.r

--- src/plot_scoring_profiles.r
+++ src/plot_scoring_profiles.r
@@ -1,11 +1,11 @@
-# Install necessary packages if not already installed
+## Installing ggplot2 library in R if not already installed
 if (!requireNamespace("ggplot2", quietly = TRUE)) {
   install.packages("ggplot2")
 }
 
 library(ggplot2)
 
-# Function to generate plots for each pair
+## Here, we create a function to generate plots for each nucleotidic pair using ggplot
 generate_plots <- function(pair_df, pair_name) {
   pair_df2 <- data.frame(
     Distance = seq(1, 20),
@@ -22,13 +22,13 @@ generate_plots <- function(pair_df, pair_name) {
           plot.background = element_rect(fill = "white"))
 }
 
-# Read the CSV file
+## Uploading the CSV files that we obtained from the obj_func_training.py
 df <- read.csv("pseudo_energy_data.csv")
 
-# Get unique pairs
+## Then we use unique to take into account only the unique nucleotide pairs
 unique_pairs <- unique(df[, c("Nucleotide.1", "Nucleotide.2")])
 
-# Generate plots for each pair
+## We then use a foor loop to generate plots for each pair
 for (i in 1:nrow(unique_pairs)) {
   pair <- unique_pairs[i, ]
   pair_df <- subset(df, Nucleotide.1 == pair$Nucleotide.1 & Nucleotide.2 == pair$Nucleotide.2)
